1568ec6009f1f1b8e03ab06d115cc82c3ce5108f
ICYou <joseph@seeyouu.nl>
2015-05-04 11:55:52 +0200

    Triangle

---
+++ GraphicsPractical1/GraphicsPractical1/Game1.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace GraphicsPractical1
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private FrameRateCounter frameRateCounter;
+        private BasicEffect effect;
+        private VertexPositionColor[] vertices;
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            this.frameRateCounter = new FrameRateCounter(this);
+            this.Components.Add(this.frameRateCounter);
+
+        }
+
+  
+        protected override void Initialize()
+        {
+            this.graphics.PreferredBackBufferWidth = 800;
+            this.graphics.PreferredBackBufferHeight = 600;
+            this.graphics.IsFullScreen = false;
+            this.graphics.SynchronizeWithVerticalRetrace = false;
+            this.graphics.ApplyChanges();
+            this.IsFixedTimeStep = false;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            this.effect = new BasicEffect(this.GraphicsDevice);
+            this.setupVertices();
+            this.effect.VertexColorEnabled = true;
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            this.Window.Title = "Graphics Tutorial | FPS: " + this.frameRateCounter.FrameRate;
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            foreach (EffectPass pass in this.effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+            }
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            this.GraphicsDevice.Clear(Color.DarkSlateBlue);
+            this.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, this.vertices, 0, 1,
+VertexPositionColor.VertexDeclaration);
+            base.Draw(gameTime);
+        }
+
+        private void setupVertices()
+        {
+            this.vertices = new VertexPositionColor[3];
+            this.vertices[0].Position = new Vector3(0f, 0f, 0f);
+            this.vertices[0].Color = Color.Red;
+            this.vertices[1].Position = new Vector3(10f, 10f, 0f);
+            this.vertices[1].Color = Color.Yellow;
+            this.vertices[2].Position = new Vector3(10f, 0f, -5f);
+            this.vertices[2].Color = Color.Green;
+        }
+
+    }
+}
